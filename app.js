893b287414521bb3e259d274a69a05dced79beec
hasan5323 <ibrahimhasan002@gmail.com>
2023-12-20 05:48:43 +0700

    1.0.0

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
+//express
 const express = require('express');
-const errorController = require('./controllers/error');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 const app = express();
 
+// view engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
+//express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+//routes and controllers import
+const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
-app.listen(3000,()=>{`listening on`});
+// listener
+const port = 3000
+app.listen(port,()=>{
+    console.log(`server listening on localhost port ${port}`);
+});
--- controllers/shop.js
+++ controllers/shop.js
@@ -11,13 +11,14 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  res.redirect('/products')
+  // Product.fetchAll(products => {
+  //   res.render('shop/index', {
+  //     prods: products,
+  //     pageTitle: 'Shop',
+  //     path: '/'
+  //   });
+  // });
 };
 
 exports.getCart = (req, res, next) => {
